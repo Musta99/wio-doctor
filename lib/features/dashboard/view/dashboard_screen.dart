2f63cc393be0c44e7911ce36f966c9cb8b0df14f
Musta99 <mustafizurrahman1403099@gmail.com>
2026-01-26 20:00:05 +0600

    feat: Patient Roaster

--- lib/features/dashboard/view/dashboard_screen.dart
+++ lib/features/dashboard/view/dashboard_screen.dart
@@ -11,150 +11,225 @@ class DashboardScreen extends StatelessWidget {
       body: Container(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            children: [
-              // -------------------Doctor Details --------------
-              ShadCard(
-                padding: EdgeInsets.all(12),
-                child: Row(
+          child: SingleChildScrollView(
+            child: Column(
+              spacing: 12,
+              children: [
+                // -------------------Doctor Details --------------
+                ShadCard(
+                  padding: EdgeInsets.all(12),
+                  child: Row(
+                    spacing: 12,
+                    children: [
+                      CircleAvatar(
+                        radius: 35,
+                        backgroundImage: NetworkImage(
+                          "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                        ),
+                      ),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Dr. Alex Riveira",
+                              style: GoogleFonts.exo2(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              "Cardiologist",
+                              style: GoogleFonts.exo2(
+                                fontSize: 16,
+                                color: Colors.teal,
+                              ),
+                            ),
+
+                            Text(
+                              "WIO ID: XXXXXXXXX",
+                              style: GoogleFonts.exo2(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                // ----------------- Appointment Summary -----------
+                Row(
                   spacing: 12,
                   children: [
-                    CircleAvatar(
-                      radius: 35,
-                      backgroundImage: NetworkImage(
-                        "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                    Expanded(
+                      child: ShadCard(
+                        padding: EdgeInsets.all(12),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+
+                          children: [
+                            Text(
+                              "Patients",
+                              style: GoogleFonts.exo2(fontSize: 16),
+                            ),
+
+                            Text(
+                              "1256",
+                              style: GoogleFonts.exo2(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+
+                            Text(
+                              "Total",
+                              style: GoogleFonts.exo2(
+                                fontSize: 12,
+                                color: Colors.green,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Dr. Alex Riveira",
-                            style: GoogleFonts.exo2(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            "Cardiologist",
-                            style: GoogleFonts.exo2(
-                              fontSize: 16,
-                              color: Colors.teal,
-                            ),
-                          ),
 
-                          Text(
-                            "WIO ID: XXXXXXXXX",
-                            style: GoogleFonts.exo2(fontSize: 16),
-                          ),
-                        ],
+                    Expanded(
+                      child: ShadCard(
+                        padding: EdgeInsets.all(12),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Today",
+                              style: GoogleFonts.exo2(fontSize: 16),
+                            ),
+
+                            Text(
+                              "12",
+                              style: GoogleFonts.exo2(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+
+                            Text(
+                              "Scheduled",
+                              style: GoogleFonts.exo2(
+                                fontSize: 12,
+                                color: Colors.teal,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+
+                    Expanded(
+                      child: ShadCard(
+                        padding: EdgeInsets.all(12),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Pending",
+                              style: GoogleFonts.exo2(fontSize: 16),
+                            ),
+
+                            Text(
+                              "4",
+                              style: GoogleFonts.exo2(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+
+                            Text(
+                              "Critical",
+                              style: GoogleFonts.exo2(
+                                fontSize: 12,
+                                color: Colors.red,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ],
                 ),
-              ),
 
-              // ----------------- Appointment Summary -----------
-              Row(
-                spacing: 12,
-                children: [
-                  Expanded(
-                    child: ShadCard(
-                      padding: EdgeInsets.all(12),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-
-                        children: [
-                          Text(
-                            "Patients",
-                            style: GoogleFonts.exo2(fontSize: 16),
+                // ----------- Today's Patient Roaster -------------
+                Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Today's Patients Roaster",
+                          style: GoogleFonts.exo2(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
                           ),
+                        ),
 
-                          Text(
-                            "1256",
-                            style: GoogleFonts.exo2(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
+                        Text(
+                          "See All",
+                          style: GoogleFonts.exo2(
+                            fontSize: 16,
+                            color: Colors.teal,
+                            fontWeight: FontWeight.w600,
                           ),
-
-                          Text(
-                            "Total",
-                            style: GoogleFonts.exo2(
-                              fontSize: 12,
-                              color: Colors.green,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                  ),
 
-                  Expanded(
-                    child: ShadCard(
-                      padding: EdgeInsets.all(12),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("Today", style: GoogleFonts.exo2(fontSize: 16)),
+                    //
+                    ListView.builder(
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemCount: 5,
+                      itemBuilder: (context, index) {
+                        return ShadCard(
+                          padding: EdgeInsets.all(12),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              CircleAvatar(radius: 35),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "Sarah Jenkins",
+                                    style: GoogleFonts.exo2(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Text(
+                                    "42y . Female . Hypertension",
+                                    style: GoogleFonts.exo2(fontSize: 12),
+                                  ),
+                                ],
+                              ),
 
-                          Text(
-                            "12",
-                            style: GoogleFonts.exo2(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
+                              ShadButton(
+                                backgroundColor: Colors.teal,
+                                child: Text(
+                                  "View",
+                                  style: GoogleFonts.exo2(fontSize: 12),
+                                ),
+                              ),
+                            ],
                           ),
-
-                          Text(
-                            "Scheduled",
-                            style: GoogleFonts.exo2(
-                              fontSize: 12,
-                              color: Colors.teal,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
+                        );
+                      },
                     ),
-                  ),
-
-                  Expanded(
-                    child: ShadCard(
-                      padding: EdgeInsets.all(12),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Pending",
-                            style: GoogleFonts.exo2(fontSize: 16),
-                          ),
-
-                          Text(
-                            "4",
-                            style: GoogleFonts.exo2(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-
-                          Text(
-                            "Critical",
-                            style: GoogleFonts.exo2(
-                              fontSize: 12,
-                              color: Colors.red,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
