59522ce9afe818434a25921989ceb0fab2dff2c4
Musta99 <mustafizurrahman1403099@gmail.com>
2026-02-17 13:22:39 +0600

    featured: fetch doctor data method has been iniated and implemented for dashboard

--- lib/features/dashboard/view_model/dashboard_view_model.dart
+++ lib/features/dashboard/view_model/dashboard_view_model.dart
@@ -1,16 +1,28 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class DashboardViewModel extends ChangeNotifier {
-  Future fetchDoctorData() async{
-    try{
+  Future fetchDoctorData() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? doctorId = prefs.getString("doctorId");
 
-      FirebaseFirestore.instance.collection("doctors").doc().get();
-
-    }catch(err){
-
-    }finally{
-
+    if (doctorId == null) {
+      Fluttertoast.showToast(
+        msg: "No doctorId found",
+        backgroundColor: Colors.red,
+        gravity: ToastGravity.BOTTOM,
+      );
     }
+    try {
+      final DocumentSnapshot snapshot =
+          await FirebaseFirestore.instance
+              .collection("doctors")
+              .doc(doctorId)
+              .get();
+      print(snapshot.data);
+    } catch (err) {
+    } finally {}
   }
 }
