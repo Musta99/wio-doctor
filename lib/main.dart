eed110b243468a343def9ec3ea05f4744ab45c18
Musta99 <mustafizurrahman1403099@gmail.com>
2026-02-23 13:02:09 +0600

    featured: profile view model has been iniated to the main function

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,7 @@ import 'package:wio_doctor/features/auth/view_model/signup_viewmodel.dart';
 import 'package:wio_doctor/features/bottom_nav_bar/view/bottom_nav_bar.dart';
 import 'package:wio_doctor/features/dashboard/view_model/dashboard_view_model.dart';
 import 'package:wio_doctor/features/patient/view_model/patient_view_model.dart';
+import 'package:wio_doctor/features/profile/view_model/profile_view_model.dart';
 import 'package:wio_doctor/features/schedule/view_model/schedule_view_model.dart';
 import 'package:wio_doctor/view_model/auth_provider.dart';
 import 'package:wio_doctor/view_model/date_picker_view_model.dart';
@@ -67,7 +68,8 @@ class AppContent extends StatelessWidget {
         ChangeNotifierProvider(create: (context) => PatientViewModel()),
         ChangeNotifierProvider(create: (context) => DatePickerProvider()),
         ChangeNotifierProvider(create: (context) => ScheduleViewModel()),
-        ChangeNotifierProvider(create: (context)=> AppointmentViewModel())
+        ChangeNotifierProvider(create: (context) => AppointmentViewModel()),
+        ChangeNotifierProvider(create: (context) => ProfileViewModel()),
       ],
       builder: (context, child) {
         return ShadApp(
